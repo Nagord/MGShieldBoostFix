b909aef4f8c6b1565668194dd18c2a2511b27e79
DragonFire47 <1redyoung@gmail.com>
2021-03-03 16:44:28 -0500

    Add project files.

---
+++ MGShieldBoostFix/Patch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace MGShieldBoostFix
+{
+    [HarmonyPatch(typeof(PLWarpDriveProgram), "FinalLateAddStats")]
+    class Patch
+    {
+        static void Postfix(PLWarpDriveProgram __instance, PLShipStats inStats)//The devs were sill and added every number nececary but forgot to actually change the shield numbers
+        {
+            if (Time.time - (float)__instance.GetType().GetField("SuperShieldBooster_LastActivationTime", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(__instance) < (float)__instance.GetType().GetField("SuperShieldBooster_ActiveTime", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(__instance))
+            {
+                //PulsarPluginLoader.Utilities.Logger.Info("Working");
+                float DontReflectOnItTooMuch = (float)__instance.GetType().GetField("SuperShieldBooster_BoostAmount", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(__instance);
+                inStats.ShieldsChargeRate += DontReflectOnItTooMuch;
+                inStats.ShieldsChargeRateMax += DontReflectOnItTooMuch;
+            }
+        }
+    }
+
+}
---
+++ MGShieldBoostFix/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using PulsarPluginLoader;
+
+namespace MGShieldBoostFix
+{
+    public class Plugin : PulsarPlugin
+    {
+        public override string Version => "1.0.0";
+
+        public override string Author => "Dragon";
+
+        public override string LongDescription => "Fixes the dev's mistake";
+
+        public override string Name => "MGShieldBoostFix";
+
+        public override string HarmonyIdentifier()
+        {
+            return "Dragon.MGShieldBoostFix";
+        }
+    }
+}
